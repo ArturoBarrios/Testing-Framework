35f25cd433472f315c73b90f64d6ad0112d10393
JarretLogan <jarretlogan20@gmail.com>
2019-03-28 19:43:59 -0400

    airport test

--- cdct_framework/lib/api_call.rb
+++ cdct_framework/lib/api_call.rb
@@ -3,16 +3,19 @@ module ApiCall
   #TODO Fill in the code for the PUt, POST, and DELETE
   #TODO use a switch statement or the send method to improve this code
   def call_service url, call_type, body
-    if call_type == 'GET'
-      agent = Mechanize.new
-      agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    agent = Mechanize.new
+    agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    case call_type
+    when 'GET'
       agent.get(url)
-    elsif call_type == 'PUT'
-
-    elsif call_type == 'POST'
-
-    elsif call_type == 'DELETE'
-
+    when 'PUT'
+      agent.put(url)
+    when 'POST'
+      agent.post(url)
+    when 'DELETE'
+      agent.delete(url)
+    else
+      "Not valid call type"
     end
   end
 end
