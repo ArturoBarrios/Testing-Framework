29050d90a9904c0da73826a2d2137840f2337030
Kefan <yang.3949@osu.edu>
2019-04-01 19:26:18 -0400

    changes

--- Testing-Framework-master/cdct_framework/spec/verify_spec.rb
+++ Testing-Framework-master/cdct_framework/spec/verify_spec.rb
@@ -252,7 +252,7 @@ describe 'test' do
     #Retrieve the values from the JSON file
     url = json_payload['service_url']
     call_type = json_payload['request_type']
-    request_body = json_payload['request_body']
+    request_body = json_payload['request_body'].to_json
 
     #Make the service call to the URL that was defined in the JSON file
     service_response = ApiCall.call_service(url, call_type,request_body)
@@ -270,7 +270,7 @@ describe 'test' do
     #Retrieve the values from the JSON file
     url = json_payload['service_url']
     call_type = json_payload['request_type']
-    request_body = json_payload['request_body']
+    request_body = json_payload['request_body'].to_json
 
     #Make the service call to the URL that was defined in the JSON file
     service_response = ApiCall.call_service(url, call_type,request_body)
@@ -278,6 +278,7 @@ describe 'test' do
     #Compare the service response to the expected response
     expect(service_response.code).to eq(json_payload['expected_response_code'])
     expect(JSON.parse(service_response.body)).to eq(json_payload['expected_response_body'])
+    ApiCall.call_service("http://127.0.0.1:8000/myapp/airports/999",'DELETE',{})
   end
 
   it 'can delete an airport correctly' do
